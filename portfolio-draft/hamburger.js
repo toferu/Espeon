516fad6133f7d523f50058949172dfd23ac18aad
Christopher Anderson <tophermichaelson@gmail.com>
2022-11-04 16:53:18 -0400

    added resume iframe

--- portfolio-draft/hamburger.js
+++ portfolio-draft/hamburger.js
@@ -1,34 +1,13 @@
-// const $hamburger = $('.hamburger');
-// const $navMenu = $('.nav-menu');
-
-// $( () => {
-
-// $hamburger.on('click', () => {
-//     $('.bar').toggleClass('active')
-//     $('.nav-item').toggleClass('active')
-// })
-
-// // $('.nav-link').forEach(n => n.on('click', () => {
-// //     $hamburger.removeClass('active')
-// //     $navMenu.removeClass('active')
-// // }) )
 
 
-// })
-window.addEventListener('load',(event) => {
+$( () => {
+//////Hamburger Event Handler///////
+const $hamburger = $('.hamburger');
+const $navMenu = $('.nav-menu');
 
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+$hamburger.on('click', () => {
+    $hamburger.toggleClass('active')
+    $navMenu.toggleClass('active')
 })
-
-hamburger.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
 
 })
